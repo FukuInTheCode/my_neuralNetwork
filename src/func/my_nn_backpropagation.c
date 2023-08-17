77486766c923e0a11bf4c22ca409c101a0f2c945
FukuInTheCode <tomc.code@gmail.com>
2023-08-17 22:56:08 +0200

    backpropagation added

--- src/func/my_nn_backpropagation.c
+++ src/func/my_nn_backpropagation.c
@@ -5,6 +5,17 @@ static double relu_grad(double x) {
     return 0.0;
 }
 
+static inline __attribute__((always_inline)) void calc_dz(my_nn_t *N, my_matrix_t *dZ, uint8_t i)
+{
+    my_matrix_t tmp = {.m = 0, .n = 0};
+    my_matrix_t tmp2 = {.m = 0, .n = 0};
+    my_matrix_transpose(&(N->theta_arr[i - 1]), &tmp);
+    my_matrix_product(&tmp2, 2, dZ, &tmp);
+    my_matrix_applyfunc(&(N->activations[i - 1]), relu_grad, &tmp);
+    my_matrix_product_elementwise(dZ, 2, &tmp, &tmp2);
+    my_matrix_free(2, &tmp, &tmp2);
+}
+
 void my_nn_backpropagation(my_nn_t *N, my_matrix_t *inputs, my_matrix_t *Y)
 {
     my_nn_forwardpropagation(N, inputs);
@@ -15,16 +26,14 @@ void my_nn_backpropagation(my_nn_t *N, my_matrix_t *inputs, my_matrix_t *Y)
     my_matrix_t tmp = {.m = 0, .n = 0};
     my_matrix_t tmp2 = {.m = 0, .n = 0};
     for (uint8_t i = N->layers_size - 1; i > 0; i--) {
+        my_matrix_print(1, &dZ);
         my_matrix_transpose(&(N->activations[i - 1]), &tmp);
         my_matrix_product(&tmp2, 2, &tmp, &dZ);
         my_matrix_multiplybyscalar(&tmp2, 1.0/(double)(Y->m), &(N->gradientsTheta[i - 1]));
-        my_matrix_sumrow(&dZ, &tmp);
+        my_matrix_sumcol(&dZ, &tmp);
         my_matrix_multiplybyscalar(&tmp, 1.0/(double)(Y->m), &(N->gradientsBias[i - 1]));
         if (i == 1) continue;
-        my_matrix_transpose(&(N->theta_arr[i - 1]), &tmp);
-        my_matrix_product(&tmp2, 2, &dZ, &tmp);
-        my_matrix_applyfunc(&(N->activations[i - 1]), relu_grad, &tmp);
-        my_matrix_product_elementwise(&dZ, 2, &tmp, &tmp2);
+        calc_dz(N, &dZ, i);
     }
     my_matrix_free(4, &dZ, &negY, &tmp, &tmp2);
 }
