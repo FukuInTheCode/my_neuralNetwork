92254941b8246370920b33dfcc82d425536e3a78
FukuInTheCode <tomc.code@gmail.com>
2023-08-17 14:23:33 +0200

    free add

--- src/func/my_nn_error_mse.c
+++ src/func/my_nn_error_mse.c
@@ -1,8 +1,14 @@
 #include "../../includes/my.h"
 
+static double __square(double x) {
+    return x * x;
+}
+
 double my_nn_calcerror_mse(my_nn_t *N, my_matrix_t *inputs, my_matrix_t *Y)
 {
     my_nn_forwardpropagation(N, inputs);
     my_matrix_t *predictions = &(N->activations[N->layers_size - 1]);
-    my_matrix_print(1, predictions);
+    my_matrix_t negY = {.m = 0, .m = 0};
+    my_matrix_multiplybyscalar(Y, -1, &negY);
+    my_matrix_free(1, &negY);
 }
--- src/main.c
+++ src/main.c
@@ -2,10 +2,16 @@
 
 int main(int argc, char* argv[])
 {
+    srand(time(NULL));
     my_matrix_t features = {.m = 0, .n = 0};
+    my_matrix_t needed = {.m = 0, .n = 0};
+    my_matrix_t targets = {.m = 0, .n = 0};
     my_matrix_create(10, 2, 1, &features);
+    my_matrix_create(2, 1, 1, &needed);
     my_matrix_randint(-1, 1, 1, &features);
-    my_matrix_print(1, &features);
+    my_matrix_randint(1, 1, 1, &needed);
+    my_matrix_product(&targets, 2, &features, &needed);
+    my_matrix_print(2, &features, &targets);
     uint8_t layers[] = {2, 3, 1};
     my_nn_t N = {.layers_size = 3, .layers = layers};
     my_nn_create(&N);
@@ -13,8 +19,7 @@ int main(int argc, char* argv[])
 
     my_matrix_print_array(&(N.theta_arr), N.layers_size - 1);
     my_matrix_print_array(&(N.bias_arr), N.layers_size - 1);
-    my_nn_forwardpropagation(&N, &features);
-    my_matrix_print_array(&(N.activations), N.layers_size);
+    my_nn_calcerror_mse(&N, &features, &targets);
 
     my_nn_free(&N);
     my_matrix_free(1, &features);
