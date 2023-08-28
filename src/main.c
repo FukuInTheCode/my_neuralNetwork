91a3502c02f817f0753984866d9b61b14fe3d61a
FukuInTheCode <tomc.code@gmail.com>
2023-08-28 22:34:18 +0200

    comments added

--- src/main.c
+++ src/main.c
@@ -67,6 +67,8 @@ int main(int argc, char* argv[])
     my_matrix_t layers_w[n_layers];
     my_matrix_t layers_b[n_layers];
 
+    // matrices decla
+
     MAT_DECLA(tmp);
     MAT_DECLA(w1);
     MAT_DECLA(b);
@@ -74,6 +76,8 @@ int main(int argc, char* argv[])
     MAT_DECLA(targets);
     MAT_DECLA(predictions);
     MAT_DECLA(diff);
+
+    // data craetion
 
     my_matrix_create(4, 2, 1, &tmp);
     my_matrix_fill_from_array(&tmp, and_train_fea, 8);
@@ -86,6 +90,8 @@ int main(int argc, char* argv[])
     my_matrix_transpose(&tmp, &targets);
     MAT_PRINT(targets);
 
+    // parameters init
+
     my_matrix_create(1, 2, 1, &w1);
     my_matrix_randfloat(-1, 1, 1, &w1);
     my_matrix_create(1, 1, 1, &b);
@@ -95,11 +101,15 @@ int main(int argc, char* argv[])
     // MAT_PRINT(b);
     // MAT_PRINT_DIM(b);
 
+    // predictions
+
     my_matrix_product(&tmp, 2, &w1, &features);
     my_matrix_add(&predictions, 2, &tmp, &b);
     MAT_PRINT(predictions);
 
     my_matrix_multiplybyscalar(&targets, -1, &tmp);
+
+    // mse calculation
 
     my_matrix_add(&diff, 2, &tmp, &predictions);
 
@@ -109,8 +119,6 @@ int main(int argc, char* argv[])
     double sum = my_matrix_sum(&tmp);
     sum /= (double)targets.n;
     printf("sum = %lf\n", sum);
-
-
 
     my_matrix_free(1, &w1);
 
