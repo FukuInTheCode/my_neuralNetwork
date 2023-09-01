0f359fbc4588feb84f1c7e94ce7f5456142f3800
FukuInTheCode <tomc.code@gmail.com>
2023-09-01 18:41:41 +0200

    train func memory gestion done

--- src/func/my_nn_train.c
+++ src/func/my_nn_train.c
@@ -14,6 +14,8 @@ void my_nn_train(my_nn_t *nn, my_matrix_t *x, my_matrix_t *y, my_params_t *hp)
             my_matrix_multiplybyscalar(&(nn->gradients_bias[i]), -1 * hp->alpha, &tmp);
             my_matrix_copy(&(nn->bias_arr[i]), &cpy);
             my_matrix_add(&(nn->bias_arr[i]), 2, &cpy, &tmp);
+            MAT_FREE(tmp);
+            MAT_FREE(copy);
         }
     }
 }
