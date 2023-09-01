6b7575f6b4eebad5f799fc76881d5922fadf1fc2
FukuInTheCode <tomc.code@gmail.com>
2023-09-01 23:42:06 +0200

    move linear grad  to linear file

--- src/func/my_nn_linear.c
+++ src/func/my_nn_linear.c
@@ -4,3 +4,9 @@ double my_nn_linear(double x)
 {
     return x;
 }
+
+double my_nn_linear_grad(double x)
+{
+    if (x == 0) return 1;
+    return x / x;
+}
--- src/func/my_nn_linear_grad.c
+++
@@ -1,7 +0,0 @@
-#include "../../includes/my.h"
-
-double my_nn_linear_grad(double x)
-{
-    if (x == 0) return 1;
-    return x / x;
-}
