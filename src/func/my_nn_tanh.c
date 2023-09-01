ff473b434f350dc361db5984514578cac9a82897
FukuInTheCode <tomc.code@gmail.com>
2023-09-01 23:43:38 +0200

    move tanh grad to tanh file

--- src/func/my_nn_tanh.c
+++ src/func/my_nn_tanh.c
@@ -4,3 +4,8 @@ double my_nn_tanh(double x)
 {
     return tanh(x);
 }
+
+double my_nn_tanh_grad(double x)
+{
+    return 1. - pow(tanh(x), 2);
+}
--- src/func/my_nn_tanh_grad.c
+++
@@ -1,6 +0,0 @@
-#include "../../includes/my.h"
-
-double my_nn_tanh_grad(double x)
-{
-    return 1. - pow(tanh(x), 2);
-}
