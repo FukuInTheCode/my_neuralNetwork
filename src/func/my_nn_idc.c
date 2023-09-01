71636162f6e82016b0bfa5d7f6a58183f22b1ced
FukuInTheCode <tomc.code@gmail.com>
2023-09-01 22:56:16 +0200

    identité courvé added

---
+++ src/func/my_nn_idc.c
@@ -0,0 +1,11 @@
+#include "../../includes/my.h"
+
+double my_nn_idc(double x)
+{
+    return (sqrt(pow(x, 2) + 1.) - 1.) / 2. + x
+}
+
+double my_nn_idc_grad(double x)
+{
+    return x / (2. * sqrt(pow(x, 2) + 1.)) + 1.
+}
