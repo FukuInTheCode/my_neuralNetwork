8e10ff8946de988c342dbcafd13cc5df04f67018
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 22:15:16 +0200

    error func cs

--- src/func/my_nn_calc_error.c
+++ src/func/my_nn_calc_error.c
@@ -8,26 +8,15 @@ static double square(double x)
 double my_nn_calc_error(my_nn_t *nn, my_matrix_t *x, my_matrix_t *y)
 {
     MAT_DECLA(p);
-
     my_nn_predict(nn, x, &p);
-
     MAT_DECLA(neg_y);
-
     my_matrix_multiplybyscalar(y, -1, &neg_y);
-
     MAT_DECLA(diff);
-
     my_matrix_add(&diff, 2, &p, &neg_y);
-
     MAT_DECLA(diff_squared);
-
     my_matrix_applyfunc(&diff, square, &diff_squared);
-
     double res = my_matrix_sum(&diff_squared);
-
     res /= y->n;
-
     my_matrix_free(4, &p, &neg_y, &diff, &diff_squared);
-
     return res;
 }
