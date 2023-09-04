bd227a8e979ffef80968eddc7678dd81a200914a
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 22:19:38 +0200

    train func cs

--- src/func/my_nn_train.c
+++ src/func/my_nn_train.c
@@ -1,6 +1,22 @@
 #include "../../includes/my.h"
 
-uint32_t my_nn_train(my_nn_t *nn, my_matrix_t *x, my_matrix_t *y, my_params_t *hp)
+static void change_gradients(my_nn_t *nn, uint32_t i, my_params_t *hp)
+{
+    MAT_DECLA(tmp);
+    my_matrix_multiplybyscalar(&(nn->gradients_theta[i]), -1 *\
+                                                hp->alpha, &tmp);
+    MAT_DECLA(cpy);
+    my_matrix_copy(&(nn->theta_arr[i]), &cpy);
+    my_matrix_add(&(nn->theta_arr[i]), 2, &cpy, &tmp);
+    my_matrix_multiplybyscalar(&(nn->gradients_bias[i]), -1 *\
+                                                hp->alpha, &tmp);
+    my_matrix_copy(&(nn->bias_arr[i]), &cpy);
+    my_matrix_add(&(nn->bias_arr[i]), 2, &cpy, &tmp);
+    my_matrix_free(2, &tmp, &cpy);
+}
+
+uint32_t my_nn_train(my_nn_t *nn, my_matrix_t *x, my_matrix_t *y,\
+                                                    my_params_t *hp)
 {
     uint32_t j = 0;
     for (j = 0; j < hp->epoch; ++j) {
@@ -8,15 +24,7 @@ uint32_t my_nn_train(my_nn_t *nn, my_matrix_t *x, my_matrix_t *y, my_params_t *h
             break;
         my_nn_backprogation(nn, x, y);
         for (uint32_t i = 0; i < nn->size - 1; ++i) {
-            MAT_DECLA(tmp);
-            my_matrix_multiplybyscalar(&(nn->gradients_theta[i]), -1 * hp->alpha, &tmp);
-            MAT_DECLA(cpy);
-            my_matrix_copy(&(nn->theta_arr[i]), &cpy);
-            my_matrix_add(&(nn->theta_arr[i]), 2, &cpy, &tmp);
-            my_matrix_multiplybyscalar(&(nn->gradients_bias[i]), -1 * hp->alpha, &tmp);
-            my_matrix_copy(&(nn->bias_arr[i]), &cpy);
-            my_matrix_add(&(nn->bias_arr[i]), 2, &cpy, &tmp);
-            my_matrix_free(2, &tmp, &cpy);
+            change_gradients(nn, i, hp);
         }
     }
     return j;
