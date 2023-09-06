e08150c74b28d75d3fa4b6ff7d87f18ffd439ef3
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 23:05:51 +0200

    bug fix

--- src/func/my_nn_train.c
+++ src/func/my_nn_train.c
@@ -47,6 +47,7 @@ uint32_t my_nn_train(my_nn_t *nn, my_matrix_t *x, my_matrix_t *y,\
             change_gradients(nn, i, hp);
         }
     }
-    printf("%*s\n", 60, " ");
+    if (hp->show_tqdm)
+        printf("%*s\n", 60, " ");
     return j;
 }
