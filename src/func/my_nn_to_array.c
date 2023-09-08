7af97cb66855aae4d8f4082585b5e78ab0c4d9de
FukuInTheCode <tomc.code@gmail.com>
2023-09-08 23:06:21 +0200

    bug fix

--- src/func/my_nn_to_array.c
+++ src/func/my_nn_to_array.c
@@ -6,14 +6,14 @@ void my_nn_to_array(my_nn_t *nn, double **res)
     for (uint32_t i = 0; i < nn->size - 1; ++i) {
         double *arr = malloc(sizeof(double) * nn->theta_arr[i].m * nn->theta_arr[i].n);
         my_matrix_to_array(&(nn->theta_arr[i]), &arr);
-        for (uint32_t j = k; j < nn->theta_arr[i].m * nn->theta_arr[i].n; ++j)
-            (*res)[j] = arr[j];
+        for (uint32_t j = 0; j < nn->theta_arr[i].m * nn->theta_arr[i].n; ++j)
+            (*res)[k + j] = arr[j];
         free(arr);
         k += nn->theta_arr[i].m * nn->theta_arr[i].n;
         arr = malloc(sizeof(double) * nn->bias_arr[i].m * nn->bias_arr[i].n);
         my_matrix_to_array(&(nn->bias_arr[i]), &arr);
-        for (uint32_t j = k; j < nn->bias_arr[i].m * nn->bias_arr[i].n; ++j)
-            (*res)[j] = arr[j];
+        for (uint32_t j = 0; j < nn->bias_arr[i].m * nn->bias_arr[i].n; ++j)
+            (*res)[k + j] = arr[j];
         k += nn->bias_arr[i].m * nn->bias_arr[i].n;
         free(arr);
     }
